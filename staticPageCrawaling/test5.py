6d3be5079b99e97af5cf0aa999a4ef4b888f40da
Layton <jihwan8807@naver.com>
2024-05-09 01:25:04 +0900

    advanced crawler

--- staticPageCrawaling/test5.py
+++ staticPageCrawaling/test5.py
@@ -1,20 +1,27 @@
+import os
+os.system('pip install --upgrade selenium')
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+import random
 
 options = Options()
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
 options.add_argument('--start-maximized')
 options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=options)
+random_sec = random.uniform(3, 5)
 
 url = 'https://www.naver.com'
 
 driver.get(url)
-time.sleep(3)
+time.sleep(random_sec)
 # 1
 # query = driver.find_element(By.ID, 'query')
 # query.send_keys('인공지능')
@@ -26,10 +33,10 @@ time.sleep(3)
 # 2
 query = driver.find_element(By.ID, 'query')
 query.send_keys('인공지능')
-time.sleep(2)
+time.sleep(random_sec)
 
 query.send_keys(Keys.ENTER)
-time.sleep(2)
+time.sleep(random_sec)
 
 driver.save_screenshot('naver_인공지능.png')
 
